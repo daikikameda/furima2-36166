ec53a1570408a1c99bb237e813c11315c2fada7d
daikikameda <daiki.d.0124@gmail.com>
2021-11-09 06:23:12 +0900

    修正

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname               {"test"}
+    email                  {"test@test.com"}
+    password               {"111111a"}
+    password_confirmation  {password}
+    first_name             {"山田"}
+    last_name              {"太郎"}
+    kana_first_name        {"ヤマダ"}
+    kana_last_name         {"タロ"}
+    birthday               {"1977-12-25"}
+  end
+end
