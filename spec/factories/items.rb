a028bfbecaaaed32a7efb4938010c402ca425a3e
daikikameda <daiki.d.0124@gmail.com>
2021-11-19 19:45:57 +0900

    商品一覧機能の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
   factory :item do
-    item_name {Faker::Lorem.sentence}
-    item_explanation {Faker::Lorem.sentence}
-    item_category_id {Faker::Number.between(from: 1, to: 3)}
-    item_condition_id {Faker::Number.between(from: 1, to: 3)}
-    delivery_fee_id {Faker::Number.between(from: 1, to: 3)}
-    delivery_area_id {Faker::Number.between(from: 1, to: 3)}
-    delivery_date_id {Faker::Number.between(from: 1, to: 3)}
-    price {"333333"}
+    item_name { Faker::Lorem.sentence }
+    item_explanation { Faker::Lorem.sentence }
+    item_category_id { Faker::Number.between(from: 1, to: 3) }
+    item_condition_id { Faker::Number.between(from: 1, to: 3) }
+    delivery_fee_id { Faker::Number.between(from: 1, to: 3) }
+    delivery_area_id { Faker::Number.between(from: 1, to: 3) }
+    delivery_date_id { Faker::Number.between(from: 1, to: 3) }
+    price { '333333' }
     association :user
-    
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/image/sample1.png'), filename: 'sample1.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,99 +6,99 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品投稿の保存' do
-    context "商品保存ができる場合" do
-      it "全て記入されて入れば保存される" do
+    context '商品保存ができる場合' do
+      it '全て記入されて入れば保存される' do
         expect(@item).to be_valid
       end
     end
 
-    context "商品が保存できない場合" do
-      it "画像が空だと登録できない" do
+    context '商品が保存できない場合' do
+      it '画像が空だと登録できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it "商品名が空だと保存できない" do
+      it '商品名が空だと保存できない' do
         @item.item_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
-      it "商品説明が空だと保存できない" do
+      it '商品説明が空だと保存できない' do
         @item.item_explanation = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item explanation can't be blank")
       end
-      it "商品カテゴリーが選択されないと保存できない" do
+      it '商品カテゴリーが選択されないと保存できない' do
         @item.item_category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item category can't be blank")
       end
-      it "商品状態が選択されないと保存できない" do
+      it '商品状態が選択されないと保存できない' do
         @item.item_condition_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item condition can't be blank")
       end
-      it "配送料の負担が選択されないと保存できない" do
+      it '配送料の負担が選択されないと保存できない' do
         @item.delivery_fee_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
-      it "配送地域が選択されないと保存できない" do
+      it '配送地域が選択されないと保存できない' do
         @item.delivery_area_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery area can't be blank")
       end
-      it "配送日時が選択されないと保存できない" do
-        @item.delivery_date_id= ''
+      it '配送日時が選択されないと保存できない' do
+        @item.delivery_date_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
-      it "価格が空だと保存できない" do
+      it '価格が空だと保存できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it "userが紐づいてないと保存できない" do
+      it 'userが紐づいてないと保存できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
-      it "価格が300以下だと保存できない" do
-        @item.price = "299"
+      it '価格が300以下だと保存できない' do
+        @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
-      it "価格が999999以下だと保存できない" do 
-        @item.price ="100000000"
+      it '価格が999999以下だと保存できない' do
+        @item.price = '100000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
-      it "半角数字でないと保存できない" do
-        @item.price ="１１１１１"
+      it '半角数字でないと保存できない' do
+        @item.price = '１１１１１'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
-      it "カテゴリーに「---」が選択されている場合は出品できない" do
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
         @item.item_category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Item category can't be blank")
       end
-      it "商品の状態に「---」が選択されている場合は出品できない" do
+      it '商品の状態に「---」が選択されている場合は出品できない' do
         @item.item_condition_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Item condition can't be blank")
       end
-      it "配送料の負担に「---」が選択されている場合は出品できない" do
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
         @item.delivery_fee_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
-      it "発送元の地域に「---」が選択されている場合は出品できない" do
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
         @item.delivery_area_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery area can't be blank")
       end
-      it "発送までの日数に「---」が選択されている場合は出品できない" do
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
         @item.delivery_date_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
